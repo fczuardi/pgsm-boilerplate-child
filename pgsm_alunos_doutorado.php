cf60d5fa362076ce8819c469f7714d7061a4879b
Fabricio C Zuardi <fabricio@fabricio.org>
2011-11-22 13:10:27 -0200

    listar todos os alunos sem paginacao

--- pgsm_alunos_doutorado.php
+++ pgsm_alunos_doutorado.php
@@ -7,6 +7,6 @@ $page_subtitle = __('Doutorado', 'pgsm-boilerplate-child');
 $next_page_label = __('Mais Alunos');
 $list_item_type = 'pgsm_aluno';
 $post_subtype = 'doutorando';
-$post_per_page = 10;
+$post_per_page = -1;
 get_template_part( 'pgsm_people_list' ); 
 ?>
